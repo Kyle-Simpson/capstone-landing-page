f63d71835198cfd22cb25a27cccec8051677c127
Kyle Simpson <kes97@uw.edu>
2019-01-25 14:43:11 -0800

    Updates

---
+++ precache-manifest.88924e466c7577192cf6e54f1ed122fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa1ac6943c3b8a4c73d0",
+    "url": "/kes97/capstone/capstone-landing-page/static/js/runtime~main.fa1ac694.js"
+  },
+  {
+    "revision": "5ef3e430b5d18ae16a80",
+    "url": "/kes97/capstone/capstone-landing-page/static/js/main.5ef3e430.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/kes97/capstone/capstone-landing-page/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5ef3e430b5d18ae16a80",
+    "url": "/kes97/capstone/capstone-landing-page/static/css/main.7fe51e06.chunk.css"
+  },
+  {
+    "revision": "3cc9897805ee0a082b1a7b560d43b29d",
+    "url": "/kes97/capstone/capstone-landing-page/index.html"
+  }
+];
